61cae7b2b420230bb3f932f1d5dc047670117790
Kasia Laniecka <kl@parkshare.dk>
2021-02-20 16:28:26 +0100

    updated post page and target blank for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,16 @@
 import { findByTitle } from "@testing-library/react";
 
 const Footer = () => {
-    return ( 
+    return (
         <div className="footer">
             <p>Internship Blog by Kasia Laniecka</p>
             <div className="some">
-                <a href="https://www.linkedin.com/in/kasialaniecka/">LinkedIn</a>
-                <a href="https://github.com/kattata">Github</a>
-                <a href="https://www.instagram.com/kasialaniecka/">Instagram</a>
+                <a href="https://www.linkedin.com/in/kasialaniecka/" target="_blank">LinkedIn</a>
+                <a href="https://github.com/kattata" target="_blank">Github</a>
+                <a href="https://www.instagram.com/kasialaniecka/" target="_blank">Instagram</a>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Footer;
